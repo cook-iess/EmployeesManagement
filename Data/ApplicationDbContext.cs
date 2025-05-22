8be763ae9c8334081c44d6fcfb33725cc14f066b
cook-iess <ekramsiraj32@gmail.com>
2025-05-22 10:50:04 +0300

    Add models and DbSets for Banks and SystemCodes
    Added `Bank`, `SystemCode`, and `SystemCodeDetail` models in the
    `EmployeesManagement.Models` namespace. Updated `ApplicationDbContext`
    with new `DbSet` properties to manage these entities. Models include
    navigation properties and optional fields where applicable.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,5 +14,8 @@ namespace EmployeesManagement.Data
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Designation> Designations { get; set; }
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<SystemCode> SystemCodes { get; set; }
+        public DbSet<SystemCodeDetail> SystemCodesDetail { get; set; }
     }
 }
